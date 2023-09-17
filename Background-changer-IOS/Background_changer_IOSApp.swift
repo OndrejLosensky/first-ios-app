3ca87026466021c82f82fe454cdfbde47beef567
Ondrej Losensky <losenskyondrej@gmail.com>
2023-09-17 17:57:15 +0200

    Initial Commit

---
+++ Background-changer-IOS/Background_changer_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Background_changer_IOSApp.swift
+//  Background-changer-IOS
+//
+//  Created by Ondřej Losenský on 17.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct Background_changer_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
